49c9a3e973c60dd5fcf12ef58c6ab2b5006c1e30
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-04 11:45:01 -0400

    commit tree 56d4e30c6c4134c9a8e5948232fd518d7e086080

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name){   
+    return array.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(array, letter){
+    return array.filter(driver => driver.toLowerCase().substring(0, letter.length) === letter.toLowerCase());
+}
+
+function matchName(array, name){
+    return array.filter(driver => driver.name === name);
+}
